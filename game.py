fcb35a339852ca87ca26e50ad18fa208bbcbba48
Timon Bakker <timonbakker@hotmail.com>
2022-11-21 11:42:57 +0100

    Implement simple menu

---
+++ game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self):
+        print("This is the complex part...")
+        # We have to perform quite a few things here:
+        # The game consist of pokemons, whereas the pokemons consists of their own data AND moves
+        # The game can also "start" -> fight our selected pokemon against the randomly selected one
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-def main():
-    print("Hello, let's play some Pokemon!")
+validoptions = ['a', 's', 'd', 'q']
 
-main()
+
+def initializegame():
+    print("Pokemon is being loaded...")
+
+
+def startmenu():
+    print("Welcome, to Pokemon!")
+    print("Select one of the following options:\n")
+
+    print("(a) Select a Pokemon")
+    print("(s) Start the fight")
+    print("(d) List all Pokemons")
+    print("(q) Quit")
+
+    choice = input("What would you like to do?: ")
+    if choice not in validoptions:
+        print("You made an invalid choice, try again!\n\n")
+        return startmenu()
+    else:
+        if choice == 'q':
+            print("Thanks for playing!")
+            exit(1)
+        else:
+            print("Still working on this...")
+
+if __name__ == "__main__":
+    initializegame()
+    startmenu()
