38e937a8ff23244ce0f54737e18bc5c10220c3eb
Timon Bakker <timonbakker@hotmail.com>
2022-11-22 10:25:21 +0100

    Add highly simplified damage calculation

--- battle.py
+++ battle.py
@@ -2,6 +2,7 @@ class Battle:
     _player = None
     _opponent = None
     _turn = 0
+    _critical_hit_change = 0.05
 
     def __init__(self, player, opponent):
         self._player = player
@@ -36,6 +37,10 @@ class Battle:
     def switch_turn(self):
         # Toggle between 0 and 1
         self._turn = (self.turn + 1) % 2
+
+    def calculate_damage(self):
+        # Use a max() to make sure we don't return a negative damage
+        return max(0, self._player.get_attack() * 2 - self._opponent.get_defense())
 
     def is_finished(self):
         return self._player.get_health() == 0 or self._opponent.get_health() == 0
--- pokemon.py
+++ pokemon.py
@@ -1,6 +1,7 @@
 class Pokemon:
     _name = ""
     _type = ""
+    _max_health = 0
     _health = 0
     _attack = 0
     _defense = 0
@@ -9,16 +10,12 @@ class Pokemon:
     def __init__(self, name, type, health, attack, defense, speed):
         self._name = name
         self._type = type
-        self._health = health
         self._attack = attack
         self._defense = defense
         self._speed = speed
 
     def get_name(self):
         return self._name
-
-    def get_stats(self):
-        return {'health': self._health, 'attack': self._attack, 'defense': self._defense, 'speed': self._speed}
 
     def get_health(self):
         return self._health
