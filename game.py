96ce502522b617ce34ecafe1f84a610ac66e5cbf
Timon Bakker <timonbakker@hotmail.com>
2022-11-22 10:49:53 +0100

    Implement Pokemon reader

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import random
+import csv
 from battle import Battle
+from pokemon import Pokemon
 
 
 class Game:
@@ -9,22 +11,40 @@ class Game:
     _opponent_pokemon = None
 
     def __init__(self):
+        self.initialize_pokemon()
         self._battle = Battle()
 
     def initialize_pokemon(self):
-        pass
-        # Get all Pokémon, store into list
+        with open('pokemon.csv', mode='r') as file:
+            content = csv.DictReader(file, delimiter=';')
+            for pokemon in content:
+                self.add_pokemon(pokemon)
+
+    def add_pokemon(self, data):
+        # All data is provided as a strings -> cast the relevant values to an int
+        try:
+            health = int(data.get('health'))
+            attack = int(data.get('attack'))
+            defense = int(data.get('defense'))
+            speed = int(data.get('speed'))
+        except ValueError:
+            print("One or more of your input values in invalid!")
+            print(f"The issue occurs at the following Pokémon: {data.get('name')}")
+            exit(1)
+        pokemon = Pokemon(data.get('name'), data.get('type'), health, attack, defense, speed)
+        self._pokemons.append(pokemon)
 
     def select_pokemon(self):
         self.list_pokemons()
-        choice = input("\nWhich Pokémon would you like to choice? ")
+        choice = input("Which Pokémon would you like to choice? ")
         try:
             choice = int(choice)
-            if choice < 0 or choice > len(self._pokemons):
+            if choice < 1 or choice > len(self._pokemons):
                 print("This index is invalid, try again!")
                 self.select_pokemon()
             else:
-                self._user_pokemon = self._pokemons[choice]
+                # We use the -1 to make sure the Pokémons listed start with 1. instead of 0.
+                self._user_pokemon = self._pokemons[choice-1]
         except ValueError:
             print("This index is invalid, try again!")
             self.select_pokemon()
@@ -33,8 +53,10 @@ class Game:
         return self._user_pokemon if self._user_pokemon else "You haven't selected a Pokémon yet!"
 
     def list_pokemons(self):
-        for index, pokemon in self._pokemons:
-            print(f"[{index}] - {pokemon.get_name()}")
+        index = 1
+        for pokemon in self._pokemons:
+            print(f"{index} - {pokemon.get_name()}")
+            index += 1
 
     def start_battle(self):
         self.randomize_opponent()
--- pokemon.py
+++ pokemon.py
@@ -1,15 +1,16 @@
 class Pokemon:
     _name = ""
-    _type = ""
+    _category = "" # Fixme: either add support for type calculation or remove this variable
     _max_health = 0
     _health = 0
     _attack = 0
     _defense = 0
     _speed = 0
 
-    def __init__(self, name, type, health, attack, defense, speed):
+    def __init__(self, name: str, category: str, health: int, attack: int, defense: int, speed: int):
         self._name = name
-        self._type = type
+        self._category = category
+        self._health = health
         self._attack = attack
         self._defense = defense
         self._speed = speed
